e0e2783db243371cf60e35cf3736643d7a8d0c1f
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-10-31 18:17:05 -0300

    Initial commit

---
+++ prisma/migrations/20221026213355_/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'ACCEPTED', 'IN_COURSE', 'CLOSED');
+
+-- CreateEnum
+CREATE TYPE "Urgency" AS ENUM ('HIGH', 'MEDIUM', 'LOW');
+
+-- CreateTable
+CREATE TABLE "RequestTransfer" (
+    "id" TEXT NOT NULL,
+    "areaFrom" TEXT NOT NULL,
+    "areaTo" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "driverId" TEXT NOT NULL,
+    "leverOfUrgency" "Urgency" NOT NULL,
+
+    CONSTRAINT "RequestTransfer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Response" (
+    "id" TEXT NOT NULL,
+    "driverId" TEXT NOT NULL,
+    "requestId" TEXT NOT NULL,
+
+    CONSTRAINT "Response_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Driver" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Driver_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Patient" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "room" TEXT NOT NULL,
+
+    CONSTRAINT "Patient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Response_requestId_key" ON "Response"("requestId");
+
+-- AddForeignKey
+ALTER TABLE "RequestTransfer" ADD CONSTRAINT "RequestTransfer_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RequestTransfer" ADD CONSTRAINT "RequestTransfer_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "Driver"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "Driver"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "RequestTransfer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,112 @@
+import { PrismaClient, Status } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+var cors = require('cors')
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+
+//LISTA DE TODOS LOS PACIENTES
+app.get('/patients', async (req, res) => {
+    const patients = await prisma.patient.findMany()
+    res.json(patients)
+})
+
+//LISTA DE TODOS LOS DRIVERS
+app.get('/drivers', async (req, res) => {
+  const patients = await prisma.driver.findMany()
+  res.json(patients)
+})
+
+//PUBLICAR UN NUEVO PACIENTE
+app.post(`/patient`, async (req, res) => {
+    const result = await prisma.patient.create({
+      data: { ...req.body },
+    })
+    res.json(result)
+})
+
+//ESTE LO HICE DE PRUEBA, ES PARA CAMBIAR LA HABITACION DEL PACIENTE
+app.put(`/patient/:id`,async (req,res) =>{
+  try {
+    console.log("champions?")
+    const result = await prisma.patient.update({
+      where:{id: req.params.id},
+      data: {room: req.body.room}
+    })
+    res.json(result)
+  }catch(e) {
+    console.log(e)
+  }
+})
+
+//CREAR UNA SOLICITUD DE TRASLADO
+app.post(`/request`, async (req, res) => {
+    const result = await prisma.requestTransfer.create({
+      data: { ...req.body },
+    })
+    res.json(result)
+})  
+
+//PASAR UN TRASLADO DE PENDING A ACCEPTED
+app.put(`/request/accept/:id`,async (req,res) =>{
+  try {
+    console.log("champions?")
+    const result = await prisma.requestTransfer.update({
+      where:{id: req.params.id},
+      data: {status: Status.ACCEPTED}
+    })
+    res.json(result)
+  }catch(e) {
+    console.log(e)
+  }
+})
+
+//PASAR UN TRASLADO DE ACCEPTED A IN-COURSE
+app.put(`/request/start/:id`,async (req,res) =>{
+  try {
+    console.log("champions?")
+    const result = await prisma.requestTransfer.update({
+      where:{id: req.params.id},
+      data: {status: Status.IN_COURSE}
+    })
+    res.json(result)
+  }catch(e) {
+    console.log(e)
+  }
+})
+
+//PASAR UN TRASLADO DE IN-COURSE A CLOSED
+app.put(`/request/close/:id`,async (req,res) =>{
+  try {
+    console.log("champions?")
+    const result = await prisma.requestTransfer.update({
+      where:{id: req.params.id},
+      data: {status: Status.CLOSED}
+    })
+    res.json(result)
+  }catch(e) {
+    console.log(e)
+  }
+})
+
+//LISTA DE TODAS LAS SOLICITUDES
+app.get('/requests', async (req, res) => {
+    const patients = await prisma.requestTransfer.findMany()
+    res.json(patients)
+})
+
+//CREAR UN NUEVO CAMILLERO
+app.post(`/driver`, async (req, res) => {
+  const result = await prisma.driver.create({
+    data: { ...req.body },
+  })
+  res.json(result)
+}) 
+
+app.listen(8080, () =>
+  console.log('REST API server ready at: http://localhost:8080'),
+)
